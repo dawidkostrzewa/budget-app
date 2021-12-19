f2aaccec51a8d317f92be6ef5883caf0a3096539
Dawid Kostrzewa <dawid.ko1997@gmail.com>
2021-12-19 12:53:03 +0100

    tsconfig setup

--- apps/budget-app-front/src/app/home-page/home-page.component.ts
+++ apps/budget-app-front/src/app/home-page/home-page.component.ts
@@ -6,63 +6,69 @@ import { Observable } from 'rxjs';
 import { TransactionsService } from './+state/transactions.service';
 
 @Component({
-    selector: 'app-home-page',
-    template: `
-        <main>
-            <h2>Aktualny miesiac: {{ currentMonth }} {{ currentYear }}</h2>
-            <div class="summary-cards">
-                <mat-card class="color-green">
-                    <mat-card-title>Wpływy</mat-card-title>
-                    <mat-card-content>{{ expense }}</mat-card-content>
-                </mat-card>
+  selector: 'app-home-page',
+  template: `
+    <main>
+      <h2>Aktualny miesiac: {{ currentMonth }} {{ currentYear }}</h2>
+      <div class="summary-cards">
+        <mat-card class="color-green">
+          <mat-card-title>Wpływy</mat-card-title>
+          <mat-card-content>{{ expense }}</mat-card-content>
+        </mat-card>
 
-                <mat-card class="color-red">
-                    <mat-card-title>Wydatki</mat-card-title>
-                    <mat-card-content>{{ income$ | async }}</mat-card-content>
-                </mat-card>
-                <mat-card [ngClass]="result > 0 ? 'color-green' : 'color-red'">
-                    <mat-card-title>+/-</mat-card-title>
-                    <mat-card-content>{{ result }}</mat-card-content>
-                </mat-card>
-            </div>
-            <div>
-                <h2>Kategorie</h2>
-                <mat-tab-group>
-                    <mat-tab *ngFor="let cat of categoryFacade.allMainCategories$ | async" [label]="cat.name">
-                        <app-category-transactions
-                            [transactions]="
-                                this.transactionService.getAllTransactionsByMainCategoryName(cat.name) | async
-                            "
-                            [summary]="
-                                (this.transactionService.getTransactionsAmountSummaryByMainCategory(cat.id) | async) ||
-                                []
-                            "
-                        ></app-category-transactions>
-                    </mat-tab>
-                </mat-tab-group>
-            </div>
-        </main>
-    `,
-    styleUrls: ['./home-page.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush
+        <mat-card class="color-red">
+          <mat-card-title>Wydatki</mat-card-title>
+          <mat-card-content>{{ income$ | async }}</mat-card-content>
+        </mat-card>
+        <mat-card [ngClass]="result > 0 ? 'color-green' : 'color-red'">
+          <mat-card-title>+/-</mat-card-title>
+          <mat-card-content>{{ result }}</mat-card-content>
+        </mat-card>
+      </div>
+      <div>
+        <h2>Kategorie</h2>
+        <mat-tab-group>
+          <mat-tab
+            *ngFor="let cat of categoryFacade.allMainCategories$ | async"
+            [label]="cat.name"
+          >
+            <app-category-transactions
+              [transactions]="
+                this.transactionService.getAllTransactionsByMainCategoryName(
+                  cat.name
+                ) | async
+              "
+              [summary]="
+                (this.transactionService.getTransactionsAmountSummaryByMainCategory(
+                  cat.id
+                ) | async) || []
+              "
+            ></app-category-transactions>
+          </mat-tab>
+        </mat-tab-group>
+      </div>
+    </main>
+  `,
+  styleUrls: ['./home-page.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomePageComponent implements OnInit {
-    private readonly today = new Date();
-    currentMonth = this.today.toLocaleString('default', { month: 'long' });
-    currentYear = this.today.getFullYear();
+  private readonly today = new Date();
+  currentMonth = this.today.toLocaleString('default', { month: 'long' });
+  currentYear = this.today.getFullYear();
 
-    income$: Observable<number> | undefined;
-    expense = 1000;
-    result = 0;
+  income$: Observable<number> | undefined;
+  expense = 1000;
+  result = 0;
 
-    constructor(
-        public readonly transactionsFacade: TransactionsFacade,
-        public readonly categoryFacade: CategoryFacade,
-        public readonly transactionService: TransactionsService,
-        public readonly apiService: ApiService
-    ) {}
+  constructor(
+    public readonly transactionsFacade: TransactionsFacade,
+    public readonly categoryFacade: CategoryFacade,
+    public readonly transactionService: TransactionsService,
+    public readonly apiService: ApiService
+  ) {}
 
-    ngOnInit() {
-        this.income$ = this.transactionsFacade.expensesAmount$;
-    }
+  ngOnInit() {
+    this.income$ = this.transactionsFacade.expensesAmount$;
+  }
 }
